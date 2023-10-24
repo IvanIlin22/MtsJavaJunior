1aa15652671cea88f9b089df4203ea09b9d84c61
Ivan Ilyin <geniusmov@yandex.ru>
2023-10-25 00:23:47 +0300

    feature/1 add simple unit test

---
+++ student-project/src/test/java/com/mts/meta/MainTest.java
@@ -0,0 +1,12 @@
+package com.mts.meta;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    public void testSum() {
+        assertEquals(2, 1 + 1);
+    }
+}
